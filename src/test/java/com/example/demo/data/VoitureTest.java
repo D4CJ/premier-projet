4ba13cd9052ea9543846b64df7201231f8a925f7
DIMITROV Dimitar <12401320@r20311.iutv.univ-paris13.fr>
2026-05-20 11:53:19 +0200

    tp1 test5 voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -1,14 +1,12 @@
-package com.example.demo.data;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class VoitureTest {
 
     @Test
-    void testVoiture(){
+    void testVoiture() {
 
         Voiture voiture1 = new Voiture("Volkswagen", 15000);
         voiture1.setId(1);
@@ -16,9 +14,9 @@ public class VoitureTest {
         Voiture voiture2 = new Voiture("Audi", 20000);
         voiture2.setId(2);
 
-        assertTrue(voiture1.getMarque()=="Volkswagen");
-        assertTrue(voiture1.getMarque()!="Gabin");
-        assertTrue(voiture1.getId() != voiture2.getId()); 
+        assertTrue(voiture1.getMarque().equals("Volkswagen"));
+        assertTrue(!voiture1.getMarque().equals("Gabin"));
+        assertTrue(voiture1.getId() != voiture2.getId());
     }
 
 }
