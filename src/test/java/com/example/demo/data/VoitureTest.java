b3f2a76b4e9a3100585ccf2212e74a8a42c3ac25
DIMITROV Dimitar <12401320@r20311.iutv.univ-paris13.fr>
2026-05-20 11:21:55 +0200

    tp1 test voiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -8,7 +8,17 @@ import org.springframework.util.Assert;
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
+    void testVoiture(){
+
+        Voiture voiture1 = new Voiture("Volkswagen", 15000);
+        voiture1.setId(1);
+
+        Voiture voiture2 = new Voiture("Audi", 20000);
+        voiture2.setId(2);
+
+        Assert.isTrue(voiture1.getMarque().equals("Volkswagen"),"Normalement c'est Volkswagen");
+        Assert.isTrue(voiture1.getMarque()!="Gabin","Normalement c'est Volkswagen");
+        Assert.isFalse(voiture1.getId() == voiture2.getId(), "ce n'est pas les mêmes"); 
     }
 
 }
