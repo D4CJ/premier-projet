22f9ad595e491de1d786164995b3e9550b80aeba
Dimitar <yopasita93380@gmail.com>
2026-06-03 10:08:26 +0200

    tp1 test Voiture.java

---
+++ src/test/java/com/example/demo/data/StatistiqueImpl_test.java
@@ -0,0 +1,19 @@
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+//public class StatistiqueImpl_test {
+
+   // @Test
+    //void StatistiqueImpl_test_avec_des_voitures() {
+
+       // StatistiqueImpl stat1 = new StatistiqueImpl();
+
+       // Voiture voiture1 = new Voiture("Opel", 10000);
+        //Voiture voiture2 = new Voiture("Mercedes", 20000);
+       // stat1.ajouter(voiture1);
+       // stat1.ajouter(voiture2);
+  //  }
+
+//}
--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -2,21 +2,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
 public class VoitureTest {
 
     @Test
-    void testVoiture() {
+    void voiture_test() {
 
         Voiture voiture1 = new Voiture("Volkswagen", 15000);
         voiture1.setId(1);
-
         Voiture voiture2 = new Voiture("Audi", 20000);
         voiture2.setId(2);
 
         assertTrue(voiture1.getMarque().equals("Volkswagen"));
         assertTrue(!voiture1.getMarque().equals("Gabin"));
-        assertTrue(voiture1.getId() != voiture2.getId());
-    }
-
-}
+        assertTrue(voiture1.getId() != voiture2.getId()); ;}}
