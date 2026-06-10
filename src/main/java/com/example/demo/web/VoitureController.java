b95249610446f0e23a4461319457517d2263d1bf
Dimitar <yopasita93380@gmail.com>
2026-06-10 10:43:26 +0200

    mis en commentaire les ligne de voiturecontrolleur

--- src/main/java/com/example/demo/web/VoitureController.java
+++ src/main/java/com/example/demo/web/VoitureController.java
@@ -1,29 +1,29 @@
-package com.example.demo.web;
+//package com.example.demo.web;
 
-import com.example.demo.data.Voiture;
-import org.springframework.web.bind.annotation.*;
+//import com.example.demo.data.Voiture;
+//import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class VoitureController {
+//@RestController
+//public class VoitureController {
 
-    @GetMapping("/bonjour")
-    public String disBonjour(){
-        return "Bonjour";
-    }
+   // @GetMapping("/bonjour")
+   // public String disBonjour(){
+    //    return "Bonjour";
+    //}
 
-    @GetMapping("/voiture")
-    public Voiture getVoiture(){
-        Voiture v = new Voiture();
-        v.setId(1);
-        return v;
-    }
+   // @GetMapping("/voiture")
+   // public Voiture getVoiture(){
+    //    Voiture v = new Voiture();
+    //    v.setId(1);
+    //    return v;
+   // }
 
-    @PostMapping("/voiture")
-    public void creerVoiture(@RequestBody Voiture voiture) throws VoitureException {
-        if(voiture.getId() == 0){
-            throw new VoitureException();
-        }
-        System.out.println(voiture);
-    }
+   // @PostMapping("/voiture")
+   // public void creerVoiture(@RequestBody Voiture voiture) throws VoitureException {
+    //    if(voiture.getId() == 0){
+      //      throw new VoitureException();
+      //  }
+       // System.out.println(voiture);
+   // }
 
-}
+//}
